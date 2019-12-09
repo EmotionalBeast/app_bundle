c807a1996a83df66663b1f0aaa90bc5c1612f5db
yaojing <yaojing@ufotosoft.com>
2019-12-09 15:03:33 +0800

    name edit

--- app_bundle_install_win.py
+++ app_bundle_install_win.py
@@ -30,17 +30,16 @@ def get_parameter(path):
 	with open(temp) as lf:
 		jsonStr = lf.read()
 		dic = json.loads(jsonStr, strict = False)
-	adb_mac = path + dic["adb_mac"]
+	adb_win = path + dic["adb_win"]
 	jar_file = path + dic["jar_file"]
 	ks_file = path + dic["ks_file"]
 	ks_alias = dic["ks_alias"]
 	ks_pass = dic["ks_pass"]
-	return adb_mac, jar_file, ks_file, ks_alias, ks_pass
+	return adb_win, jar_file, ks_file, ks_alias, ks_pass
 
 def get_device_info():
 	subprocess.getstatusoutput("adb start-server")
 	_, model = subprocess.getstatusoutput("adb shell getprop ro.product.model")
-	model = model.replace(" ","_")
 	_, android_version = subprocess.getstatusoutput("adb shell getprop ro.build.version.release")
 	_, sdk_version = subprocess.getstatusoutput("adb shell getprop ro.build.version.sdk")
 	device_info = "_" + model + "_" + android_version + "_" + sdk_version
@@ -54,35 +53,22 @@ def get_aab_file(path):
 				aab_file = root + "/" + file
 				ex_info = file[:-4]
 				return aab_file, ex_info
-
 	
 def produce_json(jar_file, json_file, adb_file):
 	command = "java -jar " + jar_file + " get-device-spec --output=" + json_file + " --adb=" + adb_file
-	state, info = subprocess.getstatusoutput(command)
-	if state == 0:
-		print("生成json文件成功！")
-	else:
-		print("生成json文件失败！")
-		print(info)
+	info = subprocess.getstatusoutput(command)
+	print(info)
 
 def produce_apks(jar_file, adb_file, aab_file, apks_file, ks_file, ks_pass, ks_alias):
 	command = "java -jar " + jar_file + " build-apks --connected-device --adb=" + adb_file + " --bundle=" + aab_file + " --output=" + apks_file + " --ks=" + ks_file + " --ks-pass=" + ks_pass + " --ks-key-alias=" + ks_alias + " --key-pass=" + ks_pass
-	state, info = subprocess.getstatusoutput(command)
-	if state == 0:
-		print("生成apks文件成功！")
-	else:
-		print("生成apks文件失败！")
-		print(info)
+	info = subprocess.getstatusoutput(command)
+	print(info)
 
 
 def install_apks(jar_file, apks_file, adb_file):
 	command = "java -jar " + jar_file + " install-apks --apks=" + apks_file + " --adb=" + adb_file
-	state, info = subprocess.getstatusoutput(command)
-	if state == 0:
-		print("安装成功！")
-	else:
-		print("安装失败！")
-		print(info)
+	info = subprocess.getstatusoutput(command)
+	print(info)
 
 
 def clean_produce_file(path):
@@ -91,54 +77,29 @@ def clean_produce_file(path):
 		for file in files:
 			if file[-4:] == "json":
 				json_file = root + "/" + file
-				os.remove(json_file)	
+				os.remove(json_file)
 				print("删除遗留json文件成功！")
-				print("---------------------")
 			if file[-4:] == "apks":
 				apks_file = root + "/" + file
 				os.remove(apks_file)
-				print("---------------------")
 				print("删除遗留apks文件成功！")
-				
 
 
 if __name__ == "__main__":
 	path = os.path.dirname(__file__)
-	print()
-	print("当前文件路径：", path)
 	adb_file, jar_file, ks_file, ks_alias, ks_pass = get_parameter(path)
+	print(jar_file)
 	aab_file, ex_info = get_aab_file(path)
 	json_file = path + "/workspace/" + ex_info + get_device_info() + ".json"
+	print(json_file)
 	apks_file = path + "/workspace/" + ex_info + get_device_info() + ".apks"
+	print("---------------------")
 	clean_produce_file(path)
-	print("正在生成json文件......")
+	print("---------------------")
+	print("生成json文件")
 	produce_json(jar_file, json_file, adb_file)
-	print()
-	print("正在生成apks文件......")
+	print("生成apks文件")
 	produce_apks(jar_file, adb_file, aab_file, apks_file, ks_file, ks_pass, ks_alias)
-	print()
-	print("正在安装apk......")
+	print("正在安装apk.........")
 	install_apks(jar_file, apks_file, adb_file)
-	print()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	print("安装完成")
